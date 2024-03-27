031b79cfc29831421e19b0d1c0b5a560b4217338
John Pieri <jvpieri@gmail.com>
2024-03-27 15:42:37 -0400

    Initialized Reviews API.

---
+++ routes/reviews.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+const Product = require("../models/Product.model");
+const Review = require("../models/Review.model")
+
+router.get("/", async (req, res, next) => {
+    return
+});
+
+module.exports = router;
