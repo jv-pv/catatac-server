b4b00d32a06807d93e7c74d6d00c456aebd8760f
John Pieri <jvpieri@gmail.com>
2024-03-28 13:24:27 -0400

    Added logic to associate a review on a product with the user that posted it.

--- routes/reviews.js
+++ routes/reviews.js
@@ -3,15 +3,19 @@ const router = express.Router();
 
 const Product = require("../models/Product.model");
 const Review = require("../models/Review.model");
+const isAuthenticated = require("../middleware/isAuthenticated");
+const User = require("../models/User.model");
 
-router.post("/:productId", async (req, res, next) => {
+router.post("/:productId", isAuthenticated, async (req, res, next) => {
   const { title, review } = req.body;
-  const { productId } = req.params
+  const { productId } = req.params;
+  const userId = req.user._id;
   try {
     const newReview = await Review.create({
       title,
       review,
       product: productId,
+      user: userId,
     });
 
     const productToPopulate = await Product.findByIdAndUpdate(
@@ -24,10 +28,22 @@ router.post("/:productId", async (req, res, next) => {
       }
     );
 
+    const userToPopulate = await User.findByIdAndUpdate(
+      newReview.user,
+      {
+        $push: { reviews: newReview._id },
+      },
+      {
+        new: true,
+      }
+    );
+
     const populatedProduct = await productToPopulate.populate("reviews");
+    const populatedUserReview = await userToPopulate.populate("reviews");
 
     console.log("Populated product with new task ==>", populatedProduct);
-    res.json(populatedProduct);
+    console.log("Populated user review ==>", populatedUserReview);
+    res.json({populatedProduct, populatedUserReview});
   } catch (error) {
     console.error("Error creating review", error);
     res.status(500).json({ errorMsg: "Error creating review", error });
